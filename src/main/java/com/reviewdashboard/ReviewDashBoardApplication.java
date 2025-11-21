77a687ee6a7402f48bc86c5ca086b72a391b6335
Sreenivas Karthik <bandisreenivakarthik@gmail.com>
2025-11-21 16:36:42 -0500

    Making spring-boot project base

---
+++ src/main/java/com/reviewdashboard/ReviewDashBoardApplication.java
@@ -0,0 +1,11 @@
+package com.reviewdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewDashBoardApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ReviewDashBoardApplication.class, args);
+  }
+}
